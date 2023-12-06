ece680b434ee0ebb8ba2deab81017d7add24a3d1
YES312 <yes312@yandex.ru>
2023-12-07 01:55:32 +0300

    v.3.4

--- internal/services/types.go
+++ internal/services/types.go
@@ -1,6 +1,6 @@
 package services
 
-type UserAuthInfo struct {
-	Login    string `json:"login"`
-	Password string `json:"password"`
-}
+// type UserAuthInfo struct {
+// 	Login    string `json:"login"`
+// 	Password string `json:"password"`
+// }
