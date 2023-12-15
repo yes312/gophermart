fd7f52267a7312491af39f901a90b1b9248f5d47
YES312 <yes312@yandex.ru>
2023-12-15 12:31:05 +0300

    v.12.2

--- internal/database/models.go
+++ internal/database/models.go
@@ -26,7 +26,7 @@ type OrderStatus struct {
 
 type Balance struct {
 	Current  float64 `json:"current"`
-	Withdraw float64 `json:"withdraw"`
+	Withdraw float64 `json:"withdrawn"`
 }
 
 type OrderSum struct {
