7565cfa7a170546379ee6e5a436ab8e2bd353bcd
YES312 <yes312@yandex.ru>
2023-12-18 18:40:03 +0300

    v.12.13

--- internal/config/config.go
+++ internal/config/config.go
@@ -59,40 +59,20 @@ func NewConfig(flag Flags) (*Config, error) {
 		c.AccrualSysremAdress = flag.R
 	}
 
-	c.LoggerLevel = "Info"
-	// ВОПРОСМЕНТОРУ в этом месте я пытаюсь прочитать конфиг из файла key.toml в windows файл прочитается, если папка с файлом будет лежать в одной
-	// папке с main. В linux на git тестах. прочитается если папка с файлом будет лежать в корне git репозитория.
-	// как решить этот вопрос?
-	filepathStr := filepath.Join("configs", "key.toml")
-	// absPath, err := filepath.Abs(filepathStr)
-	// if err != nil {
-	// 	fmt.Println("Ошибка при получении абсолютного пути:", err)
-	// 	return &Config{}, err
-	// }
+	filepathStr := filepath.Join("configs", "config.toml")
 	_, err := toml.DecodeFile(filepathStr, &c)
 	if err != nil {
 
 		return &Config{}, err
 	}
 
-	// filepathStr = filepath.Join("configs", "config.toml")
-	// absPath, err = filepath.Abs(filepathStr)
-	// if err != nil {
-	// 	fmt.Println("Ошибка при получении абсолютного пути:", err)
-	// 	return &Config{}, err
-	// }
-
-	// _, err = toml.DecodeFile(absPath, &c)
-	// if err != nil {
-	// 	return &Config{}, err
-	// }
 	c.TokenExp = c.TokenExp * time.Hour
 	// c.MigrationsPath = "migrations"
 	// c.TokenExp = time.Hour * 999
 	// c.AccrualRequestInterval = 1
 	// c.AccuralPuttingDBInterval = 1
 	// c.NumberOfWorkers = 3
-
+	// c.LoggerLevel = "Info"
 	return &c, nil
 
 }
